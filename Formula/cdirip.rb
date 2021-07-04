e98a409197949f7103d7e7422a79af2c8083af20
Jessica Stokes <hello@jessicastokes.net>
2021-07-03 19:28:55 -0700

    Fix license for cdirip

--- Formula/cdirip.rb
+++ Formula/cdirip.rb
@@ -3,7 +3,7 @@ class Cdirip < Formula
   homepage "https://web.archive.org/web/20091027000203/http://es.geocities.com/dextstuff/cdirip/news.html"
   url "https://dcemulation.org/1-files/tools/pc/mac/cdirip-0.6.3.zip"
   sha256 "816b1abf724dcc83c7b65d7259735972102a5a60a2a006d5938ef66bebb26acc"
-  license "GPL-2.0"
+  license "GPL-2.0-only"
 
   resource "test-image" do
     url "https://dreamcast.wiki/wiki/images/1/12/GDmenu_v0.6%2824-08-2016%29.zip"
