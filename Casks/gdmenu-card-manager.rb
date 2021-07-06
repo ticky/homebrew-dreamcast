87208a25d022e07702cb96f4af2a2eab1d716fcb
Jessica Stokes <hello@jessicastokes.net>
2021-07-06 12:45:10 -0700

    gdmenu-card-manager: update to 1.3.3.1 (1.3.3 with updated permissions)

--- Casks/gdmenu-card-manager.rb
+++ Casks/gdmenu-card-manager.rb
@@ -1,19 +1,16 @@
 cask "gdmenu-card-manager" do
-  version "1.3.3"
-  sha256 "69dc9d0a172b18ea784acd8fd76277e3ae37ef9ed1b77ab034ef14dadd45cebe"
+  version "1.3.3.1" # NOTE: This is a fake version so we have a "revision" over the prior version, as the underlying release has changed
+  sha256 "364dbb3b99375e0b84d226d9e403df69c9aa4276b2ffb287144aa4146d10c842"
 
-  url "https://github.com/sonik-br/GDMENUCardManager/releases/download/#{version}/GDMENUCardManager.v#{version}-osx-x64-AppBundle.zip"
+  # TODO: Once a new release is cut, switch back to the variable version
+  # url "https://github.com/sonik-br/GDMENUCardManager/releases/download/#{version}/GDMENUCardManager.v#{version}-osx-x64-AppBundle.tar.gz"
+
+  url "https://github.com/sonik-br/GDMENUCardManager/releases/download/1.3.3/GDMENUCardManager.v1.3.3-osx-x64-AppBundle.tar.gz"
   name "GDMENUCardManager"
   desc "Manage content on SD Card for Dreamcast's GDMenu"
   homepage "https://github.com/sonik-br/GDMENUCardManager"
 
   app "GDMENUCardManager.app"
-
-  preflight do
-    # For some reason, version 1.3.3 has busted permissions; neither executable is ...executable
-    set_permissions "#{staged_path}/GDMENUCardManager.app/Contents/MacOS/GDMENU", "0755"
-    set_permissions "#{staged_path}/GDMENUCardManager.app/Contents/MacOS/GDMENUCardManager/GDMENUCardManager", "0755"
-  end
 
   uninstall quit: [
     "com.github.sonik-br.gdmenucardmanager",
