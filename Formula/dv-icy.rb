40a6e28ee38a3a0d1bcab152e9b8dbf4da96c2ed
Jessica Stokes <282113+ticky@users.noreply.github.com>
2021-07-06 22:43:30 +0000

    dv-icy: add 0.9 bottle.

--- Formula/dv-icy.rb
+++ Formula/dv-icy.rb
@@ -5,6 +5,12 @@ class DvIcy < Formula
   sha256 "4635c7af2834ad62bb29203ca7e2e67d15b723dad1e4dc1cdbc181ca289806ab"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-dreamcast/releases/download/dv-icy-0.9"
+    sha256 cellar: :any,                 catalina:     "1c1cd4f5a54474c47fb9074cd1204b2ef95a25212648169a38fcf683f0ac067c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9f7a2d44a28899ac48da894f0c4abff3b4e36b6ae8fe0a2e8a4dd6fa98b0fa92"
+  end
+
   depends_on "sdl"
 
   def install
