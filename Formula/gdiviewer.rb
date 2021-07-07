09770dd4c7d55b3551fe09e19ed116007ec2c8c7
Jessica Stokes <hello@jessicastokes.net>
2021-07-06 21:48:50 -0700

    gdiviewer: fix test

--- Formula/gdiviewer.rb
+++ Formula/gdiviewer.rb
@@ -7,6 +7,11 @@ class Gdiviewer < Formula
   license "MIT"
   head "https://github.com/boyvanamstel/gdiviewer-cli.git"
 
+  resource "test-image" do
+    url "https://github.com/sonik-br/GDMENUCardManager/raw/master/src/GDMENUCardManager.Core/tools/menu_gdi/track01.iso"
+    sha256 "3f538945a8e42cca30a0903008a5865d35d43b3a6a32bf0e0042880c3596a458"
+  end
+
   def install
     inreplace "gdiinfo.sh", "dirname $1", "dirname \"$1\""
     inreplace "gdils.sh", "/usr/local/bin/gdiinfo {}", "#{bin/"gdiinfo"} \"{}\""
@@ -15,6 +20,7 @@ class Gdiviewer < Formula
   end
 
   test do
-    assert_match "Unable to confirm device can be used", shell_output("#{bin}/gdils")
+    resource("test-image").stage testpath/"test"
+    assert_match "Hardware ID: SEGA ENTERPRISES Vendor ID: 4893 CD-ROM1/1 Media ID: JUE Media Info: 0199810 Region: MK6969 Peripheral: V0.6.0 Product Number: 20160812 Version Number: Release Date: Maker ID: GDMENU", shell_output("echo $(#{bin}/gdiinfo #{testpath}/test/track01.iso 2>/dev/null)")
   end
 end
