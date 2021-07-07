5ff5252536d805a971a1c3d30dd9c40709cbd12a
Jessica Stokes <hello@jessicastokes.net>
2021-07-06 21:06:23 -0700

    Add GDIBuilder cask

---
+++ Casks/gdi-builder.rb
@@ -0,0 +1,15 @@
+cask "gdi-builder" do
+  version "1.3"
+  sha256 "2c8de4aba80edeb7a8f0eaaba3f71d5cfe325890fa059f7a24446b27373ce05d"
+
+  url "https://github.com/Sappharad/GDIbuilder/releases/download/v#{version}/gdibuilder#{version.delete(".")}_macOS.zip"
+  name "GDMENUCardManager"
+  desc "Utility to assist with (re)building Dreamcast .gdi images from scratch"
+  homepage "https://github.com/Sappharad/GDIbuilder"
+
+  app "GDIBuilder.app"
+
+  uninstall quit: [
+    "com.sappharad.gdibuilder",
+  ]
+end
